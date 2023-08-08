810fce54838228b9b8d9e90230eb2d2a0ad433c8
Shariar Hossain <shariarhossin99@gmail.com>
2023-08-08 11:32:26 +0600

    add all file

---
+++ image_resize.py
@@ -0,0 +1,45 @@
+from PIL import Image
+import os
+
+import PIL 
+
+def resize_images(input_folder, output_folder, max_width, max_height):
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    for filename in os.listdir(input_folder):
+        input_path = os.path.join(input_folder, filename)
+        output_path = os.path.join(output_folder, filename)
+
+        if os.path.isfile(input_path):
+          
+            img = Image.open(input_path)
+
+           
+            original_width, original_height = img.size
+
+            aspect_ratio = original_width / original_height
+            new_width = min(original_width, max_width)
+            new_height = int(new_width / aspect_ratio)
+
+         
+            if new_height > max_height:
+                new_height = max_height
+                new_width = int(new_height * aspect_ratio)
+
+           
+            resized_img = img.resize((new_width, new_height), PIL.Image.Resampling.LANCZOS)
+
+
+
+            resized_img.save(output_path)
+            print(f"{input_path} -> {output_path}")
+
+if __name__ == "__main__":
+   
+    input_folder = r"C:/Users/SHARIAR/OneDrive/Desktop/Python/New folder/INPUT"
+    output_folder = r"C:/Users/SHARIAR/OneDrive/Desktop/Python/New folder/OUTPUT"
+    max_width = 32  
+    max_height = 32  
+
+    resize_images(input_folder, output_folder, max_width, max_height) 
